35127a0b1db646fe6f99e69d954fa18e77274d46
MindBlock <esveritas@gmail.com>
2015-08-12 00:42:26 +0200

    added useable items setup

--- src/com/mindblock/mazehunter/useables/UseableItems.java
+++ src/com/mindblock/mazehunter/useables/UseableItems.java
@@ -3,32 +3,67 @@ package com.mindblock.mazehunter.useables;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.mindblock.mazehunter.save.PlayerInformation;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class UseableItems {
 
+	public static final String DISTRACT = "DISTRACT";
+	public static final String JUMP = "JUMP";
+	public static final String RETURN = "RETURN";
+	public static final String REVEAL = "REVEAL";
+	public static final String INVINCIBLE = "INVINCIBLE";
+	public static final String FREEZE = "FREEZE";
 	
-	private Map<String, Integer> items;
-	private static UseableItems useableItems = null;
+	private Map<String, Boolean> items;
+	private Context context;
 	
-	private UseableItems(){
-		items = new HashMap<String, Integer>();
+	public UseableItems(Context context){
+		items = new HashMap<String, Boolean>();
+		this.context = context;
+		
+		this.loadAll();
 	}
 	
-	public static UseableItems getUseableItem(){
-		if (useableItems != null)
-			return useableItems;
-		return new UseableItems();
-	}
-	
-	public Map<String, Integer> getUseableItems(){
+	public Map<String, Boolean> getAllUseableItems(){
 		return this.items;
 	}
 	
-	public void addUseableItem(String name){
-		if (this.items.containsKey(name)){
-			this.items.put(name, this.items.get(name)+1);
-		}
-		else {
-			this.items.put(name, 1);
-		}
+	public void unlockUseableItem(String item){
+		this.items.put(item, true);
+		this.saveAll();
+	}
+	
+	public void lockUseableItem(String item){
+		this.items.put(item, false);
+		this.saveAll();
+	}
+	
+	private void loadAll(){
+		
+		SharedPreferences myPrefs = this.context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+		items.put(UseableItems.DISTRACT, myPrefs.getBoolean(UseableItems.DISTRACT, false));
+		items.put(UseableItems.JUMP, myPrefs.getBoolean(UseableItems.JUMP, false));
+		items.put(UseableItems.RETURN, myPrefs.getBoolean(UseableItems.RETURN, false));
+		items.put(UseableItems.REVEAL, myPrefs.getBoolean(UseableItems.REVEAL, false));
+		items.put(UseableItems.INVINCIBLE, myPrefs.getBoolean(UseableItems.INVINCIBLE, false));
+		items.put(UseableItems.FREEZE, myPrefs.getBoolean(UseableItems.FREEZE, false));
+	}
+	
+	private void saveAll(){
+		
+		SharedPreferences myPrefs = this.context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+		SharedPreferences.Editor e = myPrefs.edit();
+		
+		e.putBoolean(UseableItems.DISTRACT, this.items.get(UseableItems.DISTRACT));
+		e.putBoolean(UseableItems.JUMP, this.items.get(UseableItems.JUMP));
+		e.putBoolean(UseableItems.RETURN, this.items.get(UseableItems.RETURN));
+		e.putBoolean(UseableItems.REVEAL, this.items.get(UseableItems.REVEAL));
+		e.putBoolean(UseableItems.INVINCIBLE, this.items.get(UseableItems.INVINCIBLE));
+		e.putBoolean(UseableItems.FREEZE, this.items.get(UseableItems.FREEZE));
+		
+		e.commit();
 	}
 }
